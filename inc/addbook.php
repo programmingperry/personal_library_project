442443b02bb2971fd20e7d27e0b07aaac6c45afe
Nele M <nelemeyer43@gmail.com>
2025-07-02 10:17:50 +0200

    add pagetitle to addbook.php

--- inc/addbook.php
+++ inc/addbook.php
@@ -1,9 +1,12 @@
+<?php 
+$pageTitle = "Add a new Book"
+?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 <div class="container my-5" style="max-width: 600px;">
   <h1 class="mb-4 text-center">Add a new book</h1>
 
-  <form>
+  <form method="POST">
 
     <div class="mb-3">
       <label for="bookTitle" class="form-label">Book title</label>
